553de91bef3174dbb4cedbbd5c05ee494a104490
Seungkyoon Bong <seungkyoon.bong@yale.edu>
2018-06-26 15:38:26 -0400

    Begin MSE for tiles project

--- Solutions/Image_Tiling.py
+++ Solutions/Image_Tiling.py
@@ -1,2 +1,12 @@
-import PIL
+from PIL import Image, ImageDraw
 import heapq
+
+def average_color((x, y), (a, b), image):
+	r, g, b = 0, 0, 0
+	count = 0
+	for s in range(x, x+a):
+
+def MSE():
+	pass
+
+self.image = Image.open(path).convert('RGB')
